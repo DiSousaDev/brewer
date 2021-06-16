3a47e9336b8f6a6e262870a3641bf48852ec753a
Éder Diego de Sousa <me@diego.dev.br>
2021-06-16 17:06:24 -0300

    Projeto inicial

---
+++ src/test/java/br/com/diego/brewer/BrewerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.diego.brewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
