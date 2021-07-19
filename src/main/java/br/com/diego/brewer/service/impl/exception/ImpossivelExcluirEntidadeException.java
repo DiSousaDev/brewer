d9fa30db27f4b585cbbc1366b12eabcd785d20e0
Éder Diego de Sousa <me@diego.dev.br>
2021-07-19 12:35:15 -0300

    Revert ""Aula 25.1""
    This reverts commit 4f78640f

--- src/main/java/br/com/diego/brewer/service/impl/exception/ImpossivelExcluirEntidadeException.java
+++
@@ -1,9 +0,0 @@
-package br.com.diego.brewer.service.impl.exception;
-
-public class ImpossivelExcluirEntidadeException extends RuntimeException {
-    private static final long serialVersionUID = 1L;
-
-    public ImpossivelExcluirEntidadeException(String msg){
-        super(msg);
-    }
-}
